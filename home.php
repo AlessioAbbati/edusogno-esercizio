10f75de74eacf79d9b455715a28c55ae3daa2de3
AlessioAbbati <alex_117@live.it>
2023-10-02 18:32:44 +0200

    print events on personal page

--- home.php
+++ home.php
@@ -1,8 +1,16 @@
-<?php 
+<?php
+include "db_connection.php";
 session_start();
 
 if (isset($_SESSION['id']) && isset($_SESSION['nome'])) {
 
+    // Query SQL per recuperare gli eventi dell'utente autenticato
+    $userEmail = $_SESSION['email'];
+    $sql = "SELECT * FROM eventi WHERE attendees LIKE '%$userEmail%'";
+
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
 ?>
 
 <!DOCTYPE html>
@@ -17,15 +25,36 @@ if (isset($_SESSION['id']) && isset($_SESSION['nome'])) {
 </head>
 
 <body>
-    <h1>Ciao, <?php echo $_SESSION['nome']; ?> ecco i tuoi eventi</h1>
+    <h1>Ciao, <?php echo $_SESSION['nome']; ?> <?php echo $_SESSION['cognome']; ?> ecco i tuoi eventi</h1>
+
+    <?php
+        // Stampare gli eventi come card
+        while ($row = mysqli_fetch_assoc($result)) {
+    ?>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $row['nome_evento']; ?></h5>
+            <p class="card-text">Data: <?php echo $row['data_evento']; ?></p>
+            <!-- Altre informazioni sugli eventi possono essere stampate qui -->
+        </div>
+    </div>
+    <?php
+        }
+        // Rilascia la risorsa del risultato
+        mysqli_free_result($result);
+    } else {
+        echo "Errore nella query: " . mysqli_error($conn);
+    }
+    ?>
 
 </body>
 
 </html>
-
 <?php
-}else{
-    // header("location: index.php");
-    // exit();
 }
- ?>
+?>
+
+
+
+
+
