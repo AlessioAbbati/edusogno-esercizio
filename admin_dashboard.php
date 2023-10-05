d25ad1b4992fba7b9587f3bf90f911676776f01a
AlessioAbbati <alex_117@live.it>
2023-10-05 12:40:52 +0200

    mod style admin

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -61,58 +61,62 @@ $eventi = $controllerEvento->getEventi();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="assets/styles/style.css"> -->
+    <link rel="stylesheet" href="assets/styles/dashStyle.css">
     <title>Pannello di Amministrazione</title>
 </head>
 
 <body>
     <h1>Pannello di Amministrazione</h1>
-    <a href="home.php">Pagina personale</a>
+    <button><a href="home.php">Pagina personale</a></button>
 
     <?php if ($_SESSION['admin'] === true) { ?>
-        <h2>Aggiungi un Evento</h2>
-        <form action="admin_dashboard.php" method="post">
-            <input type="hidden" name="action" value="add">
-            <label>Nome Evento:</label>
-            <input type="text" name="nome_evento" required><br>
-            <label>Attendees (Email):</label>
-            <input type="email" name="attendees" required><br>
-            <label>Data e Ora dell'Evento:</label>
-            <input type="datetime-local" name="data_evento" required><br>
-            <button type="submit">Aggiungi Evento</button>
-        </form>
 
-
+        <div class="form-box">
+            <form class="form" action="admin_dashboard.php" method="post">
+                <input type="hidden" name="action" value="add">
+                <span class="title">Aggiungi un Evento</span>
+                <span class="subtitle">Compila i campi per aggiungere un evento.</span>
+                <div class="form-container">
+                    <input type="text" class="input" name="nome_evento" placeholder="Nome Evento" required>
+                    <input type="text" class="input" name="attendees" placeholder="Attendees (Email)" required>
+                    <input type="datetime-local" class="input" name="data_evento" required>
+                </div>
+                <button class="button" type="submit">Aggiungi Evento</button>
+            </form>
+        </div>
 
         <h2>Elenco Eventi</h2>
         <ul>
-    <?php foreach ($eventi as $indice => $evento) { ?>
-        <li>
-            <strong>Nome Evento:</strong> <?php echo $evento->getNomeEvento(); ?><br>
-            <strong>Attendees:</strong> <?php echo $evento->getAttendees(); ?><br>
-            <strong>Data e Ora dell'Evento:</strong> <?php echo $evento->getDataEvento(); ?><br>
-            <form action="admin_dashboard.php" method="post" style="display: inline;">
-                <input type="hidden" name="action" value="edit">
-                <input type="hidden" name="id_evento" value="<?php echo $evento->getId(); ?>">
-                <label for="nome_evento">Nuovo Nome Evento:</label>
-                <input type="text" name="nome_evento" value="<?php echo $evento->getNomeEvento(); ?>" placeholder="Nuovo Nome Evento">
-                <label for="attendees">Nuovi Attendees:</label>
-                <input type="email" name="attendees" value="<?php echo $evento->getAttendees(); ?>" placeholder="Nuovi Partecipanti">
-                <label for="data_evento">Nuova Data e Ora dell'Evento:</label>
-                <input type="datetime-local" name="data_evento" value="<?php echo date("Y-m-d\TH:i:s", strtotime($evento->getDataEvento())); ?>" placeholder="Nuova Data e Ora">
-                <button type="submit">Modifica</button>
-            </form>
+            <?php foreach ($eventi as $indice => $evento) { ?>
+                <li>
+                    <strong>Nome Evento:</strong> <?php echo $evento->getNomeEvento(); ?><br>
+                    <strong>Attendees:</strong> <?php echo $evento->getAttendees(); ?><br>
+                    <strong>Data e Ora dell'Evento:</strong> <?php echo $evento->getDataEvento(); ?><br>
+                    <div class="form-box">
+                        <form class="form" action="admin_dashboard.php" method="post">
+                            <input type="hidden" name="action" value="edit">
+                            <input type="hidden" name="id_evento" value="<?php echo $evento->getId(); ?>">
+                            <label for="nome_evento">Nuovo Nome Evento:</label>
+                            <input type="text" class="input" name="nome_evento" value="<?php echo $evento->getNomeEvento(); ?>" placeholder="Nuovo Nome Evento">
+                            <label for="attendees">Nuovi Attendees:</label>
+                            <input type="text" class="input" name="attendees" value="<?php echo $evento->getAttendees(); ?>" placeholder="Nuovi Partecipanti">
+                            <label for="data_evento">Nuova Data e Ora dell'Evento:</label>
+                            <input type="datetime-local" class="input" name="data_evento" value="<?php echo date("Y-m-d\TH:i:s", strtotime($evento->getDataEvento())); ?>" placeholder="Nuova Data e Ora">
+                            <button class="button" type="submit">Modifica</button>
+
+                        </form>
+                        <form class="form" action="admin_dashboard.php" method="post" style="display: inline;">
+                            <input type="hidden" name="action" value="delete">
+                            <input type="hidden" name="indice" value="<?php echo $indice; ?>">
+                            <input type="hidden" name="id_evento" value="<?php echo $evento->getId(); ?>">
+                            <button class="button" type="submit">Elimina</button>
+                        </form>
+                    </div>
 
 
-            <form action="admin_dashboard.php" method="post" style="display: inline;">
-                <input type="hidden" name="action" value="delete">
-                <input type="hidden" name="indice" value="<?php echo $indice; ?>">
-                <input type="hidden" name="id_evento" value="<?php echo $evento->getId(); ?>">
-                <button type="submit">Elimina</button>
-            </form>
-        </li>
-    <?php } ?>
-</ul>
+                </li>
+            <?php } ?>
+        </ul>
 
 
     <?php } else { ?>
